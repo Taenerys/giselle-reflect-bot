c03af5e276a1ee0dece1de30614cf7f172eedf46
Taenerys <thuanh99@student.fdu.edu>
2021-07-17 16:06:38 -0700

    add run script

---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cd /Users/thuanh2710/Desktop/giselle-reflect-bot
+source /Users/thuanh2710/Desktop/giselle-reflect-bot/venv/bin/activate
+python /Users/thuanh2710/Desktop/giselle-reflect-bot/giselle-tweet.py
